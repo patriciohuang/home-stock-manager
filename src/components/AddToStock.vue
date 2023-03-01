95613e21f624b74bb0f31a1a62d88cf5d794cc1b
Patricio Huang <patriciohuang20@gmail.com>
2023-03-01 16:25:10 +0100

    Add to stock component

---
+++ src/components/AddToStock.vue
@@ -0,0 +1,40 @@
+<template>
+<v-text-field
+  v-model="expirationDate"
+  label="Expiration Date"
+  type="date"
+  :error="v$.expirationDate.$error"
+></v-text-field>
+<span class="text-red" v-if="v$.expirationDate.$error">Expiration date is requred.</span>
+<v-btn color="indigo" @click="addToStock()">
+  Add to Stock
+</v-btn>
+</template>
+<script>
+import useValidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+export default {
+  emits: ['addToStock'],
+  props: {
+    item: Object
+  },
+  data: ()=> ({
+    v$: useValidate(),
+    expirationDate: ''
+  }),
+  methods: {
+    addToStock: function() {
+      console.log(this.expirationDate)
+      this.v$.$validate()
+      if(!this.v$.$error) {
+        this.$emit("addToStock", {item: this.item, date: this.expirationDate});
+      }
+    } 
+  },
+  validations() {
+    return {
+      expirationDate: { required }
+    }
+  },
+}
+</script>
