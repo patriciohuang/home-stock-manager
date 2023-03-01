62d3f7788aaedd2f93fb27453d2759854e1a6970
Patricio Huang <patriciohuang20@gmail.com>
2023-03-01 16:42:07 +0100

    suggestion list add delete function

--- src/components/SuggestionList.vue
+++ src/components/SuggestionList.vue
@@ -4,14 +4,24 @@
     <div v-else>
       <h3>Suggestions</h3>
       <v-card 
-      class="mx-auto"
+      class="mt-4"
       >
         <v-list density="compact">
-          <v-list-item 
-          v-for="item in suggestionList"
-          class="pl-6"
-          @click="addItem(item)">{{ item }}
-        </v-list-item >
+          <v-container>
+            <v-row v-for="item in suggestionList" class="d-flex justify-space-between">
+              <v-list-item 
+              class="pl-6"
+              @click="addItem(item)">{{ item }}
+              </v-list-item >
+              <v-btn
+              variant="text"
+              icon="mdi-close"
+              color="blue-grey-lighten-3"
+              @click="deleteItem(item)"
+              >
+              </v-btn>
+          </v-row>
+          </v-container>
       </v-list>
       </v-card>
     </div >
@@ -29,7 +39,12 @@ export default {
   methods: {
     addItem: function(item) {
       this.$emit("addItem", item)
-    }
+    },
+    deleteItem : function (item) {
+        const index = this.suggestionList.indexOf(item)
+        this.suggestionList.splice(index, 1);
+        localStorage.setItem('suggestionList', JSON.stringify(this.suggestionList));
+      },
   }
 }
 </script>
