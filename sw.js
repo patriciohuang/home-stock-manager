d3ab2f2e556434a24fc6de66f532f589a676d519
Patricio Huang <patriciohuang20@gmail.com>
2023-03-21 18:35:43 +0100

    remove unused changes

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        catches.open("static").then(cache => {
-            return cache.addAll(["./", "./src/assets/main.css", "./src/assets/base.css", "./statics/icon-192x192.png"]);
-        })
-    )
-});
