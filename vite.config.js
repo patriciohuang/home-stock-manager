edfa8f02dd4c99f1a8c848d2a8501e8f342426cd
Patricio Huang <patriciohuang20@gmail.com>
2023-03-21 19:00:54 +0100

    Add PWA Support

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
+import manifestOptions from './manifest-options.json'
 
 // https://vitejs.dev/config/
+
+const vitePWAOptions = { 
+  registerType: 'autoUpdate',
+  devOptions: {
+    enabled: true
+  }, 
+  manifest: manifestOptions
+}
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA(vitePWAOptions),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
